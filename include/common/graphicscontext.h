3be02fabe3ad500457e62100b2e0a3bd355c219e
Ujjwal Dixit <53943612+ujjwaldixt@users.noreply.github.com>
2025-01-20 22:33:59 -0600

    commented graphicscontext.h

--- include/common/graphicscontext.h
+++ include/common/graphicscontext.h
@@ -1,14 +1,16 @@
-#ifndef __MYOS__COMMON__GRAPHICSCONTEXT_H
-#define __MYOS__COMMON__GRAPHICSCONTEXT_H
+#ifndef __MYOS__COMMON__GRAPHICSCONTEXT_H  // Header guard to prevent multiple inclusions of this file
+#define __MYOS__COMMON__GRAPHICSCONTEXT_H  // Defines the unique macro for this header file
 
-#include <drivers/vga.h>
+#include <drivers/vga.h>  // Includes the definition/declaration of the VideoGraphicsArray class
 
 namespace myos
 {
     namespace common
     {
+        // This typedef creates a shorter alias 'GraphicsContext' for 'myos::drivers::VideoGraphicsArray'.
+        // It allows code to refer to VideoGraphicsArray functionality using 'GraphicsContext'.
         typedef myos::drivers::VideoGraphicsArray GraphicsContext;
     }
 }
 
-#endif
+#endif  // End of header guard
